3472366d1fc18a26726608ba1e379fd53a61e5ce
TVDK <85726678+tvdk-sudoku@users.noreply.github.com>
2026-02-15 19:37:22 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://sudoku-resources.netlify.app/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
